0f550013d78c95e546ef0eb654d69f55f8a5ee20
Jason Papapanagiotakis <jason.92@windowslive.com>
2014-11-14 18:06:06 +0000

    Paradoteos Kwdikas part 1

--- main.cpp
+++ main.cpp
@@ -7,101 +7,85 @@
 
 using namespace std;
 
+HashMap * createGraph(int, int);
+void destroyGraph(HashMap*);
+void insertNode(HashMap *, int);
+void insertEdge(HashMap *, int, int);
+Node * lookupNode(HashMap *, int);
+
 int main(void)
 {
 
-    if (true)
-    {
 
-
-        HashMap* xartis = new HashMap(4,4);
-        cout << "split_index = "<<xartis->split_index<<endl;
-        cout << "original_size = "<<xartis->original_size<<endl;
-        cout << "round = "<<xartis->round<<endl;
-        cout << "bucket_cells = "<<xartis->bucket_cells<<endl;
-
-        Node* komvos = new Node(1);
-        xartis->insertNode(komvos);
-        komvos = new Node(15);
-        xartis->insertNode(komvos);
-        komvos = new Node(8);
-        xartis->insertNode(komvos);
-        komvos = new Node(3);
-        xartis->insertNode(komvos);
-        komvos = new Node(7);
-        xartis->insertNode(komvos);
-        komvos = new Node(19);
-        xartis->insertNode(komvos);
-        komvos = new Node(12);
-        xartis->insertNode(komvos);
-
-        komvos = new Node(11);
-        xartis->insertNode(komvos);
-
-	for(int i  = 10000; i>100; i--)
-	{
-		komvos = new Node(i);
-        	xartis->insertNode(komvos);
-	}
-	cout << "round = "<<xartis->round<<endl;
+        HashMap* xartis = createGraph(4,4);
+        
+        insertNode(xartis,1);
+        insertNode(xartis,15);
+        insertNode(xartis,8);
+        insertNode(xartis,3);
+        insertNode(xartis,7);
+        insertNode(xartis,19);
+        insertNode(xartis,12);
+        insertNode(xartis,11);
+        
         //xartis->print();
 
-
-
-
-        cout<<endl<<"size "<<xartis->hashTable->getSize()<<endl;
         xartis->sort_map();
 
-        Edge* e = new Edge(1,NULL);
-        xartis->insertEdge(11,e);
+		insertEdge(xartis, 11, 1);
+		insertEdge(xartis, 1, 12);
+		insertEdge(xartis, 12, 3);
+		insertEdge(xartis, 3, 19);
+		insertEdge(xartis, 1, 19);
 
-        e = new Edge(12,NULL);
-        xartis->insertEdge(1,e);
-
-        e = new Edge(3,NULL);
-        xartis->insertEdge(12,e);
-
-        e = new Edge(19,NULL);
-        xartis->insertEdge(3,e);
-
-        e = new Edge(19,NULL);
-        xartis->insertEdge(1,e);
-
-        //cout<<"h apostasi einai: "<<xartis->reachNode1(1,19);
-
-        ResultSet* r = xartis->reachNodesN(11);
+        cout<< endl << endl << "Distance from 1 "<< " to " << " 3 " << " ----> " << xartis->reachNode1(1,3) << endl << endl;
+	
+		int start = 11;
+        ResultSet* r = xartis->reachNodesN(start);
         while(true)
         {
             Result* result = r->get_next();
             if(result != NULL)
             {
-                cout<< result->node_id << "   "<<result->distance<<endl;
+                cout<<"Distance from " << start << " to " << result->node_id << " -----> "<<result->distance<<endl;
                 delete result;
             }
             else
                 break;
         }
         delete(r);
-
-        //result = r->get_next();
-        //result->print();
-
+        
         cout<<endl;
 
-        //xartis->print();
+        xartis->print();
 
-        delete xartis;
+        destroyGraph(xartis);
 
-    }
-    else
-    {
-        list* l = new list();
-        Node* komvos = new Node(1);
-        l->Add(komvos);
-        listIterator* it = l->getIterator();
-        Node* n = (Node*)it->getData();
-        cout<<n->get_id()<<endl;
-
-    }
-	//list* liiiiist = new list();
+	
 }
+
+HashMap * createGraph(int buckets, int bucket_size)
+{
+		HashMap* xartis = new HashMap(buckets,bucket_size);
+}
+void insertNode(HashMap * xartis,int _id)
+{
+	 Node* komvos = new Node(_id);
+     xartis->insertNode(komvos);
+}
+void insertEdge(HashMap * xartis, int from, int to)
+{
+	Edge* e = new Edge(to,NULL);
+    xartis->insertEdge(from,e);
+}
+Node * lookupNode(HashMap * xartis, int _id)
+{
+	return xartis->lookupNode(_id);
+}
+void destroyGraph(HashMap * xartis)
+{
+	delete xartis;
+}
+
+
+
